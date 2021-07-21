ca4ff1d56709e1dfd55c19c7c429596ef20abc56
abhinavdhamaniya <abhinavdhamaniya@gmail.com>
2021-07-22 02:53:46 +0530

    Removed unused import

--- src/app/import-excel/import-data/import-data.service.ts
+++ src/app/import-excel/import-data/import-data.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { CompanyDto } from "src/app/dto/CompanyDto";
 
 @Injectable({
     providedIn: 'root'
